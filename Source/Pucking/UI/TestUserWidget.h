e07ac0e9ec005f69448c4f7909d719f4c18a0dc8
jaemin-han <duddjakgkrl@gmail.com>
2024-12-18 13:32:45 +0900

    폴더 구조 만들기
    UI 폴더 내부에는 각자 적절히 폴더를 만들어서 사용하시길 바랍니다. 나중에 TestUserWidget 은 삭제하도록 하겠습니다.

---
+++ Source/Pucking/UI/TestUserWidget.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TestUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUCKING_API UTestUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
