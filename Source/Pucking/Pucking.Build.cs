dca59514db11899a961d256451a7f679fdbe6e3b
jaemin-han <duddjakgkrl@gmail.com>
2024-12-18 16:56:54 +0900

    include 경로 설정

--- Source/Pucking/Pucking.Build.cs
+++ Source/Pucking/Pucking.Build.cs
@@ -6,6 +6,8 @@ public class Pucking : ModuleRules
 {
 	public Pucking(ReadOnlyTargetRules Target) : base(Target)
 	{
+		PublicIncludePaths.AddRange(new string[] { "Pucking" });
+		
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG" });
