dca93fb6f39899910bc4ee0abcf079e41653bfe8
jaemin-han <duddjakgkrl@gmail.com>
2024-12-31 13:54:40 +0900

    Animation BluePrint 파일 재생성 AnimIntsnacen 파일 생성

---
+++ Source/Pucking/Character/PuckAnimInstance.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PuckAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUCKING_API UPuckAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+};
