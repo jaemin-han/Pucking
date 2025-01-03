67bdab8982251cf54d6ba378fe3dbaf4fe6ee4e6
WoohuiJ <woohui1996@naver.com>
2025-01-03 15:09:53 +0900

    save

--- Source/Pucking/Character/PuckingCharacter.cpp
+++ Source/Pucking/Character/PuckingCharacter.cpp
@@ -61,7 +61,7 @@ void APuckingCharacter::BeginPlay()
 	// Call the base class  
 	Super::BeginPlay();
 
-	
+	Tags.Add(FName("Player"));
 }
 
 //////////////////////////////////////////////////////////////////////////
--- Source/Pucking/Enemy/EnemyBase.h
+++ Source/Pucking/Enemy/EnemyBase.h
@@ -6,12 +6,15 @@
 #include "GameFramework/Character.h"
 #include "EnemyBase.generated.h"
 
+
 UENUM(BlueprintType)
 enum class EEnemyState : uint8
 {
+	EES_Dead UMETA(DisplayName = "Dead"),
 	EES_Patrolling UMETA(DisplayName = "Patrolling"),
 	EES_Chasing UMETA(DisplayName = "Chasing"),
-	EES_Attacking UMETA(DisplayName = "Attacking")
+	EES_Attacking UMETA(DisplayName = "Attacking"),
+	EES_Engaged UMETA(DisplayName = "Engaged")
 };
 
 UCLASS()
@@ -22,51 +25,99 @@ class PUCKING_API AEnemyBase : public ACharacter
 public:
 	// Sets default values for this character's properties
 	AEnemyBase();
+	
+	UPROPERTY(EditAnywhere)
+	class UPawnSensingComponent* PawnSensingComp;
+	
+	UPROPERTY(EditAnywhere)
+	class UEnemyStatusComponent* StatusComp;
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+protected:
+	//
+	//Navigation
+	//
 	void CheckPatrolTarget();
+	void CheckCombatTarget();
 	AActor* ChoosePatrolTarget();
 	void PatrolTimerFinished();
 	void MoveToTarget(AActor* Target);
 	bool InTargetRange(AActor* Target, float Radius);
 
+	//
+	//Take Hit
+	//
 	void Die();
-	void CheckCombatTarget();
 	void DirectionalHitReact(const FVector& ImpactPoint);
-
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
+	void HideHealthBar();
+	void ShowHealthBar();
+	
+	//
+	//Attack
+	//
+	void Attack();
+	void PlayAttackMontage();
+	void LoseInterest();
+	void StartPatrolling();
+	void ChaseTarget();
+	void StartAttackTimer();
+	void ClearAttackTimer();
+	bool CanAttack();
+	
+	//
+	//Sense
+	//
 	UFUNCTION()
 	void PawnSeen(APawn* SeenPawn);
+
 protected:
 	UPROPERTY()
 	class AAIController* EnemyController;
+
+	UPROPERTY(BlueprintReadOnly)
+	EEnemyState EnemyState = EEnemyState::EES_Patrolling;
+	
+	UPROPERTY(EditInstanceOnly, Category = "Combat")
+	AActor* CombatTarget;
+	FTimerHandle AttackTimer;
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	float AttackMin = 0.5f;
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	float AttackMax = 1.f;
 	
 	UPROPERTY(EditInstanceOnly, Category = "AI Navigation")
 	AActor* PatrolTarget;
 	UPROPERTY(EditInstanceOnly, Category = "AI Navigation")
 	TArray<AActor*> PatrolTargets;
-		
-	UPROPERTY(EditAnywhere, Category = "AI Navigation")
-	float WaitMin = 5.f;
-	UPROPERTY(EditAnywhere, Category = "AI Navigation")
-	float WaitMax = 10.f;
-
-	UPROPERTY(EditAnywhere)
-	double PatrolRadius = 200.f;
-	
 	FTimerHandle PatrolTimer;
-
+	UPROPERTY(EditAnywhere, Category = "AI Navigation")
+	float WaitMin = 2.f;
+	UPROPERTY(EditAnywhere, Category = "AI Navigation")
+	float WaitMax = 4.f;
+	
 	UPROPERTY(EditAnywhere)
-	class UPawnSensingComponent* PawnSensingComp;
+	float PatrolAcceptanceRadius = 200.f;
+	UPROPERTY(EditAnywhere)
+	float CombatRadius = 1000.f;
+	UPROPERTY(EditAnywhere)
+	float AttackRadius = 150.f;
+	UPROPERTY(EditAnywhere)
+	float WalkSpeed = 125.f;
+	UPROPERTY(EditAnywhere)
+	float RunSpeed = 300.f;
 
-	EEnemyState EnemyState = EEnemyState::EES_Patrolling;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UAnimMontage* AttackMontage;
+
+	UPROPERTY()
+	TSubclassOf<class UHealthBarComponent> HealthBarClass;
+
+	UPROPERTY()
+	UHealthBarComponent* HealthBarWidget;
 };
